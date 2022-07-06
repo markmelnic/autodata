da6f4db8e63c6dce5399e4b5a96aeb577c33201a
Mark <markhappy200@gmail.com>
2022-07-06 11:27:38 +0300

    Multiple exception handlers & data pickup on startup

--- aevs.py
+++ aevs.py
@@ -1,8 +1,9 @@
 import logging
+from typing import final
 logging.basicConfig(level=logging.INFO, format="%(asctime)s: %(message)s")
 
 
-import time, enum, random, json
+import os, enum, json, time
 from matplotlib.pyplot import text
 
 from selenium import webdriver
@@ -11,7 +12,7 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
-from selenium.common.exceptions import NoSuchElementException
+from selenium.common.exceptions import NoSuchElementException, WebDriverException
 
 
 class LinksEnum(enum.Enum):
@@ -29,8 +30,14 @@ class AEVS:
         self.dv = webdriver.Chrome(
             chrome_options=chrome_options, executable_path=r"chromedriver.exe"
         )
+        self._break = True
 
-        self.indexed = {}
+        if 'data.json' in os.listdir():
+            with open('data.json', 'r') as f:
+                self.indexed = json.load(f)
+        else:
+            self.indexed = {}
+
         self._current_make = None
         self._current_model = None
         self._current_generation = None
@@ -39,61 +46,98 @@ class AEVS:
     def scrape(self, ):
         self.dv.get(LinksEnum.base.value)
 
-        _el_cars = self.dv.find_elements(By.CLASS_NAME, "carman")
-        cars = [c.find_element(By.TAG_NAME, "h5") for c in _el_cars]
-        cars = [(
-                    c.find_element(By.TAG_NAME, "span").text,
-                    c.find_element(By.TAG_NAME, "a").get_attribute("href"),
-                ) for c in cars]
+        while self._break:
+            try:
+                _el_cars = self.dv.find_elements(By.CLASS_NAME, "carman")
+                cars = [c.find_element(By.TAG_NAME, "h5") for c in _el_cars]
+                cars = [(
+                            c.find_element(By.TAG_NAME, "span").text,
+                            c.find_element(By.TAG_NAME, "a").get_attribute("href"),
+                        ) for c in cars]
+                self._break = False
+            except WebDriverException:
+                pass
+            finally:
+                pass
+        self._break = True
 
         for i, car in enumerate(cars):
             self._current_make = car[0]
+            if self._current_make in self.indexed.keys():
+                continue
+
             self.indexed[self._current_make] = {}
             logging.info(f'Scraping [{i+1}/{len(cars)}] {self._current_make}')
+
             self._scrape_make(car[1])
 
             self._write_json()
 
 
     def _scrape_make(self, link: str):
-        self.dv.get(link)
+        while self._break:
+            self.dv.get(link)
 
-        _el_models = self.dv.find_elements(By.CLASS_NAME, "carmod")
-        models = [m.find_element(By.TAG_NAME, "a") for m in _el_models]
-        models = [(
-                    m.find_element(By.TAG_NAME, "h4").text[len(self._current_make)+1:],
-                    m.find_element(By.TAG_NAME, "a").get_attribute("href"),
-                ) for m in _el_models]
+            try:
+                _el_models = self.dv.find_elements(By.CLASS_NAME, "carmod")
+                models = [m.find_element(By.TAG_NAME, "a") for m in _el_models]
+                models = [(
+                            m.find_element(By.TAG_NAME, "h4").text[len(self._current_make)+1:],
+                            m.find_element(By.TAG_NAME, "a").get_attribute("href"),
+                        ) for m in _el_models]
+                self._break = False
+            except WebDriverException:
+                pass
+            finally:
+                pass
+        self._break = True
 
         for i, model in enumerate(models):
             self._current_model = model[0]
+            if self._current_make in self.indexed[self._current_make].keys():
+                continue
+
             self.indexed[self._current_make][self._current_model] = {}
             logging.info(f'   [{i+1}/{len(models)}] {self._current_model}')
             self._scrape_model(model[1])
 
+            self._write_json()
+
 
     def _scrape_model(self, link: str):
-        self.dv.get(link)
+        while self._break:
+            self.dv.get(link)
 
-        _el_generations = self.dv.find_elements(By.CLASS_NAME, "carmodel")
-        generations = [m.find_element(By.TAG_NAME, "a") for m in _el_generations]
-        generations = [(
-                        g1.find_element(By.CLASS_NAME, "col-red").text,
-                        g1.get_attribute("href"),
-                        g2.find_element(By.CLASS_NAME, "years").text,
-                        [(
-                            v.find_element(By.CLASS_NAME, "col-green2").text,
-                            v.find_element(By.TAG_NAME, "a").get_attribute("href"),
-                        ) for v in g2.find_elements(By.CLASS_NAME, "engitm")],
-                    ) for g1, g2 in zip(generations, _el_generations)]
+            try:
+                _el_generations = self.dv.find_elements(By.CLASS_NAME, "carmodel")
+                generations = [m.find_element(By.TAG_NAME, "a") for m in _el_generations]
+                generations = [(
+                                g1.find_element(By.CLASS_NAME, "col-red").text,
+                                g1.get_attribute("href"),
+                                g2.find_element(By.CLASS_NAME, "years").text,
+                                [(
+                                    v.find_element(By.CLASS_NAME, "col-green2").text,
+                                    v.find_element(By.TAG_NAME, "a").get_attribute("href"),
+                                ) for v in g2.find_elements(By.CLASS_NAME, "engitm")],
+                            ) for g1, g2 in zip(generations, _el_generations)]
+                self._break = False
+            except WebDriverException:
+                pass
+            finally:
+                pass
+        self._break = True
 
         for i, gen in enumerate(generations):
             self._current_generation = f'{gen[0]} {gen[2]}'
+            if self._current_generation in self.indexed[self._current_make][self._current_model]:
+                continue
+
             self.indexed[self._current_make][self._current_model][self._current_generation] = {}
             self.indexed[self._current_make][self._current_model][self._current_generation]['from'] = gen[2].split(' - ')[0]
             self.indexed[self._current_make][self._current_model][self._current_generation]['to'] = gen[2].split(' - ')[1]
             self.indexed[self._current_make][self._current_model][self._current_generation]['variants'] = []
             logging.info(f'      {self._current_generation}')
+
             for variant in gen[3]:
                 self._current_variant = variant[0]
                 logging.info(f'         {self._current_variant}')
@@ -101,30 +145,45 @@ class AEVS:
 
 
     def _scrape_variant(self, link: str):
-        self.dv.get(link)
-        self.dv.refresh()
+        while self._break:
+            self.dv.get(link)
 
-        variant = {}
-        variant['engine'] = self._current_variant
-        specs = self.dv.find_elements(By.CLASS_NAME, "techdata")
+            try:
+                self.dv.refresh()
+                self._break = False
+            except WebDriverException:
+                time.sleep(3)
+        self._break = True
 
-        for spec in specs:
-            spec_name = spec.find_element(By.CLASS_NAME, "title").text
-            if spec_name == '':
-                continue
-            if '\u2013' in spec_name:
-                spec_name = spec_name.split(' \u2013 ')[0]
-            variant[spec_name] = {}
+        while self._break:
+            try:
+                variant = {}
+                variant['engine'] = self._current_variant
+                specs = self.dv.find_elements(By.CLASS_NAME, "techdata")
 
-            cols1 = spec.find_elements(By.TAG_NAME, "dt")
-            cols2 = spec.find_elements(By.TAG_NAME, "dd")
-            for c1, c2 in zip(cols1, cols2):
-                if '\n' in c2.text:
-                    variant[spec_name][c1.text] = c2.text.split('\n')
-                else:
-                    variant[spec_name][c1.text] = c2.text
+                for spec in specs:
+                    spec_name = spec.find_element(By.CLASS_NAME, "title").text
+                    if spec_name == '':
+                        continue
+                    if '\u2013' in spec_name:
+                        spec_name = spec_name.split(' \u2013 ')[0]
+                    variant[spec_name] = {}
 
-        self.indexed[self._current_make][self._current_model][self._current_generation]['variants'].append(variant)
+                    cols1 = spec.find_elements(By.TAG_NAME, "dt")
+                    cols2 = spec.find_elements(By.TAG_NAME, "dd")
+                    for c1, c2 in zip(cols1, cols2):
+                        if '\n' in c2.text:
+                            variant[spec_name][c1.text] = c2.text.split('\n')
+                        else:
+                            variant[spec_name][c1.text] = c2.text
+
+                self.indexed[self._current_make][self._current_model][self._current_generation]['variants'].append(variant)
+                self._break = False
+            except WebDriverException:
+                pass
+            finally:
+                pass
+        self._break = True
 
 
     def _write_json(self, ):
